626fb405fd2c12a39f25cc040cb4a7e9684b6917
Avishkar Gaikwad <staravishkar840@gmail.com>
2023-01-09 17:07:33 +0530

    New Develovement for Mongo DB

--- Shopping.API/Data/ProductContext.cs
+++ Shopping.API/Data/ProductContext.cs
@@ -1,4 +1,6 @@
-﻿using Shopping.API.Models;
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Shopping.API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,31 @@ using System.Threading.Tasks;
 
 namespace Shopping.API.Data
 {
-    public static class ProductContext
+    public class ProductContext
     {
-        public static readonly List<Product> Products = new List<Product>
+        public ProductContext(IConfiguration configuration)
         {
-            new Product()
+            var client = new MongoClient(configuration["DatabaseSettings:ConnectionString"]);
+            var database = client.GetDatabase(configuration["DatabaseSettings:DatabaseName"]);
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+            SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; set; }
+
+        private static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(p => true).Any();
+            if (!existProduct)
+            {
+                productCollection.InsertManyAsync(GetPreconfiguredProducts());
+            }
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>() 
+            {
+                new Product()
                 {
                     Name = "IPhone X",
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
@@ -59,6 +81,11 @@ namespace Shopping.API.Data
                     Category = "Home Kitchen"
                 }
 
-        };
+            };
+
+       
+        }
+
+        
     }
 }
